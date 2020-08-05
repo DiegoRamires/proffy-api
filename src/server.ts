50e195c7c78a4455a5a1f3e544894f34492296d2
DiegoRamires <goncalves.ramires@gmail.com>
2020-08-05 00:20:39 -0300

    feature: install and enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost/${port}`);
