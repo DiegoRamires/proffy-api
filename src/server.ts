1ab5c5a5c13bd3283b5d09495181029d203e7d5c
DiegoRamires <goncalves.ramires@gmail.com>
2020-08-08 19:49:14 -0300

    bugfix: fix cors error

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import cors from "cors";
 const app = express();
 const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost/${port}`);
